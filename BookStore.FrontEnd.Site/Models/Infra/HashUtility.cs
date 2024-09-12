525c16c6f6d080574ad8c85294818dc8e4bea68a
atin4455 <atinzulin@hotmail.com>
2024-09-12 10:51:12 +0800

    新增HashUtility類別

---
+++ BookStore.FrontEnd.Site/Models/Infra/HashUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BookStore.FrontEnd.Site.Models.Infra
+{
+    public static class HashUtility
+    {
+        public static string GetSalt()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["Salt"];
+        }
+
+        public static string ToSHA256(string plainText, string salt = null)
+        {
+            salt = salt ?? GetSalt(); // 若 salt 是 null 就取得 GetSalt() 的值
+
+            using (var mySHA256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(plainText + salt); // 把密碼加上 salt 並轉成 byte[]
+                var hash = mySHA256.ComputeHash(bytes);//進行SHA256加密
+
+                var sb = new StringBuilder(); //用來存放加密後的字串
+                foreach (var b in hash)
+                {
+                    sb.Append(b.ToString("X2"));  //把byte轉成16進位字字串,字母是大寫
+                }
+
+                return sb.ToString(); // 回傳加密後的字串
+            }
+        }
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,8 @@
-﻿[working on]建立EFModels
+﻿[V]建立EFModels
 add/Models/EFModels/folder
 建立AppDbContext,Connection String,Entity Classes
+---------------
+會員系統
+[working on]add 註冊新會員
+add/Models/Infra/HashUtility.cs
+add AppSetting,key=....
