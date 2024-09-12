1c323b8fba7ae2aad91ee603b3d45fcfe128850b
atin4455 <atinzulin@hotmail.com>
2024-09-12 11:45:55 +0800

    新增擴充方法

---
+++ BookStore.FrontEnd.Site/Models/ViewModels/RegisterVm.cs
@@ -0,0 +1,75 @@
+﻿using BookStore.FrontEnd.Site.Models.EFModels;
+using BookStore.FrontEnd.Site.Models.Infra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.FrontEnd.Site.Models.ViewModels
+{
+    public class RegisterVm
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "帳號")]
+        [Required]
+        [StringLength(30)]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 明碼
+        /// </summary>
+        [Display(Name = "密碼")]
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "確認密碼")]
+        [Required]
+        [StringLength(50)]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "姓名")]
+        [Required]
+        [StringLength(30)]
+
+        public string Name { get; set; }
+
+        [Display(Name = "手機")]
+        [StringLength(10)]
+
+        public string Mobile { get; set; }
+
+    }
+
+    public static class RegisterExt
+    {
+        public static Member ToMember(this RegisterVm vm)
+        {
+            var salt = HashUtility.GetSalt();
+            var hashPassword = HashUtility.ToSHA256(vm.Password, salt); //建立密碼的雜湊值
+            var confirmCode = Guid.NewGuid().ToString("N"); // 發送確認信時使用
+
+            return new Member
+            {
+                Id = vm.Id, //這行可有可無,用不到
+                Account = vm.Account,
+                EncryptedPassword = hashPassword,
+                Email = vm.Email,
+                Name = vm.Name,
+                Mobile = vm.Mobile,
+                IsConfirmed = false, //新會員預設狀態是:未確認
+                ConfirmCode = confirmCode
+            };
+        }
+    }
+}
